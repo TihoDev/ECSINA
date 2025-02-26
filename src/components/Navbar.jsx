d98824e8e0f24d53d8c067f57ca1953ec77d5edd
Abolfazl <abolfazl84abz@gmail.com>
2025-02-27 02:31:09 +0330

    Add some styles to Navbar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 import BaseIcon from "./icon/BaseIcon";
-
+// bg-gradient-to-bl from-[#FEF1FD] to-[#FFFFFF]
 function Navbar() {
   return (
-    <div className="bg-section-background w-full h-[90px] rounded-b-[81px] shadow-2xl flex justify-between items-center px-10">
+    <div className="container bg-section-background relative h-[90px] rounded-b-[81px] shadow-2xl flex justify-between items-center px-10">
       <BaseIcon id="Grow" size={21} disableGradient={true} fillColor="#000" />
-      <nav className="w-[171px] h-[556px] px-7 bg-gradient-to-bl from-[#FEF1FD] to-[#FFFFFF] flex flex-col space-y-5 absolute top-0 right-0 font-semibold text-[10px] rounded-bl-[50px]">
-        {/* <span className="text-left mt-5">+</span> */}
-        <BaseIcon className="text-left mt-5" id="Close" disableGradient={true} fillColor="#3E243C" size={50}/>
+      <nav className="w-[171px] h-[556px] px-7 bg-red-300 flex flex-col space-y-5 fixed top-0 right-0 font-semibold text-[10px] rounded-bl-[50px] transform translate-x-0 transition-all">
+        <span className="text-left mt-5 text-lg">X</span>
         <ul className="space-y-4">
           <li>محصولات ما</li>
           <li>صفحه اصلی</li>
