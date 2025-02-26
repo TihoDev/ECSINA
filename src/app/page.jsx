f73558b095c13de28a8f4bfdb37873c6a1ee36a4
Abolfazl <abolfazl84abz@gmail.com>
2025-02-27 01:24:36 +0330

    add close icon

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,10 +3,12 @@ import BelowMainPageBanner from "@/components/BelowMainPageBanner";
 import HeroSection from "@/components/HeroSection";
 import MainPageBanner from "@/components/MainPageBanner";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <HeroSection />
       <MainPageBanner />
       <BelowMainPageBanner />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,11 @@ import BaseIcon from "./icon/BaseIcon";
 
 function Navbar() {
   return (
-    <div className="bg-section-background h-[90px] rounded-b-[81px] shadow-2xl flex justify-between items-center px-10">
+    <div className="bg-section-background w-full h-[90px] rounded-b-[81px] shadow-2xl flex justify-between items-center px-10">
       <BaseIcon id="Grow" size={21} disableGradient={true} fillColor="#000" />
-      <nav className="w-[171px] h-[556px] px-5 bg-gray-400 flex flex-col space-y-5 absolute top-0 right-0 font-semibold text-[10px] rounded-bl-[50px]">
-        <span className="text-left mt-5">X</span>
+      <nav className="w-[171px] h-[556px] px-7 bg-gradient-to-bl from-[#FEF1FD] to-[#FFFFFF] flex flex-col space-y-5 absolute top-0 right-0 font-semibold text-[10px] rounded-bl-[50px]">
+        {/* <span className="text-left mt-5">+</span> */}
+        <BaseIcon className="text-left mt-5" id="Close" disableGradient={true} fillColor="#3E243C" size={50}/>
         <ul className="space-y-4">
           <li>محصولات ما</li>
           <li>صفحه اصلی</li>
