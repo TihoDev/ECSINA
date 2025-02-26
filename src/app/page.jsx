6a19e1a1938b6f9d3789812d2fd6aec6b5e06ff7
Cena Babajannjhad <cenababajannjhad@gmail.com>
2025-02-27 00:30:00 +0330

    remove some useless style from classname

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,15 @@
 import BelowMainPageBanner from "@/components/BelowMainPageBanner";
 import HeroSection from "@/components/HeroSection";
 import MainPageBanner from "@/components/MainPageBanner";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="text-6xl font-black">
+    <div>
       <HeroSection />
       <MainPageBanner />
       <BelowMainPageBanner />
+      <Footer></Footer>
     </div>
   );
 }
