7ceeee52ff7873f8181b303150cdf13a052505cf
Cena Babajannjhad <cenababajannjhad@gmail.com>
2025-02-26 17:11:52 +0330

    add component our Product section

---
+++ src/components/ourProductSection.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const ourProductSection = () => {
+  return (
+    <section className="text-center space-y-8">
+    <h1 className="font-extrabold text-xl lg:text-3xl">محصولات ما</h1>
+    <div className="our-products-section p-4">
+        <div className="product-card md:odd:mt-10 md:even:mt-0">
+            hello world
+        </div>
+        <div className="product-card md:odd:mt-10 md:even:mt-0">
+            hello world
+        </div>
+        <div classNameName="product-card md:odd:mt-10 md:even:mt-0 col-span-2 md:col-span-1">
+            hello world
+        </div>
+        <div className="product-card md:odd:mt-10 md:even:mt-0">
+            hello world
+        </div>
+        <div className="product-card md:odd:mt-10 md:even:mt-0">
+            hello world
+        </div>
+    </div>
+</section>
+  )
+}
+
+export default ourProductSection
