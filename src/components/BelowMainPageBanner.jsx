d61ffb72da2cb9f0f2c82a5efb32d9af05176164
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-02-26 23:44:47 +0330

    The Responsive Below Main Page Banne is Completed

--- src/components/BelowMainPageBanner.jsx
+++ src/components/BelowMainPageBanner.jsx
@@ -3,7 +3,7 @@ import Button from "./UI/Button";
 
 const BelowMainPageBanner = () => {
   return (
-    <div className="flex flex-col items-center gap-y-10">
+    <div className="flex flex-col items-center gap-y-3 lg:gap-y-10">
       <p className=" text-title text-xs lg:text-2xl font-bold text-center leading-9">
         اکسین به عنوان دستیاری حرفه ای همیشه همراه کسب و کارتان شما است .<br />{" "}
         دغدغه ی ما هموار کردن مسیر شما است.{" "}
