ccca351e9ecd1f6a7306b9385d47e125c0687206
Cena Babajannjhad <cenababajannjhad@gmail.com>
2025-02-27 14:37:34 +0330

    finish the ourProductSection

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,31 @@
 import React from "react"
+import BaseIcon from "./icon/BaseIcon"
 
 
-function ProductCard({title,text}) {
+// props
+//  title : card title | text : card description | svg : icon name | className : custom class
+
+function ProductCard({title,text,svg,className}) {
     return (
-        <div className="w-[334px] h-[304px] rounded-4xl shadow-[0px_3px_3px_0px_#3e243c80] p-5">
-            <div className="flex flex-col justify-items-start">
-                <div className="mb-4">
-                     {/* svg is here */}
+        <div className={`${className} product-card flex flex-col justify-between`}>
+            <div>
+                <div className="flex flex-col justify-items-start">
+                    <div className="mb-4 bg-white p-2 shadow-product-card-icon rounded-lg w-fit">
+                        <BaseIcon id={`${svg}`} size={32}/>
+                    </div>
+                    <div>
+                        <h1 className="font-bold text-right text-xl leading-[35px] mb-4 text-(0,0,0,1)]">{title}</h1>
+                    </div>
                 </div>
                 <div>
-                    <h1 className="font-bold text-2xl leading-[35px] mb-4 text-(0,0,0,1)]">{title}</h1>
+                    <p className="font-normal text-right text-md leading-[31px] text-[rgba(0,0,0,1)]">{text}</p>
                 </div>
             </div>
-            <div>
-                <p className="font-normal text-[20px] leading-[31px] text-[rgba(0,0,0,1)]">{text}</p>
+
+            <div className="mr-auto bg-white w-12 h-12 shadow-md rounded-2xl mt-2">
+                <div className="w-full h-full flex items-center justify-center">
+                    <BaseIcon id="Arrow" size={18}/>
+                </div>
             </div>
         </div>
     )
--- src/components/ourProductSection.jsx
+++ src/components/ourProductSection.jsx
@@ -1,28 +1,58 @@
-import React from 'react'
+import ProductCard from "./ProductCard"
 
-const ourProductSection = () => {
+const productCardData = [
+  {
+    id : 1,
+    svg : "BusinessModel",
+    style : "col-span-1 lg:self-end",
+    title : "بیزنس مدل",
+    desc : "انواعی از بیزنس مدل ها که نشان می‌دهد یک کسب‌وکار چگونه ارزش ایجاد می‌کند."
+  },
+  {
+    id: 2,
+    svg : "BusinessCanvas",
+    style : "col-span-1 lg:self-start",
+    title : "بوم کسب و کار",
+    desc : "یکی از ابزار های مدیریت استراتژیک برای تجزیه و تحلیل و بهینه سازی مدل های کسب و کار است "
+  },
+  {
+    id : 3,
+    svg : "Proposals",
+    style : "col-span-2 lg:col-span-1 lg:self-end",
+    title : "پروپوزال ها",
+    desc : "انواعی از اسناد رسمی که برای ارائه ایده ها پروژه‌ها یا پیشنهادات به افراد، سازمان‌ها یا سرمایه‌گذاران تهیه شده اند."
+  },
+  {
+    id : 4,
+    svg : "BusinessPlan",
+    style : "col-span-1 lg:self-start",
+    title : "بیزنس پلن",
+    desc : "انواعی از سند های مکتوب  که اهداف، استراتژی‌ها، بازار هدف و برنامه‌های عملیاتی یک کسب‌وکار را مشخص می‌کند."
+  },
+  {
+    id : 5,
+    svg : "BestCanvas",
+    style : "col-span-1 lg:self-end",
+    title : "بوم ناب",
+    desc : "یکی از بهترین ابزار های مدل سازی کسب و کار که مخصوص استارتاپ ها و کسب و کار های نوپا طراحی شده است "
+  },
+]
+const OurProductSection = () => {
+
   return (
-    <section className="text-center space-y-8">
+    <section className="container text-center space-y-8">
     <h1 className="font-extrabold text-xl lg:text-3xl">محصولات ما</h1>
     <div className="our-products-section p-4">
-        <div className="product-card md:odd:mt-10 md:even:mt-0">
-            hello world
-        </div>
-        <div className="product-card md:odd:mt-10 md:even:mt-0">
-            hello world
-        </div>
-        <div classNameName="product-card md:odd:mt-10 md:even:mt-0 col-span-2 md:col-span-1">
-            hello world
-        </div>
-        <div className="product-card md:odd:mt-10 md:even:mt-0">
-            hello world
-        </div>
-        <div className="product-card md:odd:mt-10 md:even:mt-0">
-            hello world
-        </div>
+      {
+        productCardData.map(data => {
+          return (
+            <ProductCard key={data.id} title={data.title} text={data.desc} svg={data.svg} className={data.style}/>
+          )
+        })
+      }
     </div>
 </section>
   )
 }
 
-export default ourProductSection
+export default OurProductSection
