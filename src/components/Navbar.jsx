f15bfe7e5e83f63a3ece9784123eab8b29ae375c
Abolfazl <abolfazl84abz@gmail.com>
2025-02-27 17:29:50 +0330

    Implemented Mobile navbar and Main navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,22 @@
-import BaseIcon from "./icon/BaseIcon";
-// bg-gradient-to-bl from-[#FEF1FD] to-[#FFFFFF]
+import MobileNavbar from "./MobileNavbar";
+
 function Navbar() {
   return (
-    <div className="container bg-section-background relative h-[90px] rounded-b-[81px] shadow-2xl flex justify-between items-center px-10">
-      <BaseIcon id="Grow" size={21} disableGradient={true} fillColor="#000" />
-      <nav className="w-[171px] h-[556px] px-7 bg-red-300 flex flex-col space-y-5 fixed top-0 right-0 font-semibold text-[10px] rounded-bl-[50px] transform translate-x-0 transition-all">
-        <span className="text-left mt-5 text-lg">X</span>
-        <ul className="space-y-4">
+    <>
+      <div className="block md:hidden relative">
+        <MobileNavbar />
+      </div>
+      <nav className="container hidden md:flex mt-14 justify-center lg:justify-start">
+        {" "}
+        {/* Desctop navbar */}
+        <ul className="text-lg border-2 border-[#3E243C] w-[550px] lg:w-[665px] h-16 lg:h-20 px-7 flex justify-around items-center rounded-2xl [&>li]:cursor-pointer">
           <li>محصولات ما</li>
           <li>صفحه اصلی</li>
           <li>درباره ما</li>
           <li>تماس با ما</li>
         </ul>
       </nav>
-      <BaseIcon
-        id="ExinIcon2"
-        disableGradient={true}
-        size={35}
-        fillColor="#3E243C"
-      />
-    </div>
+    </>
   );
 }
 
