4de46394d8c05698a91bdd546df0de55a29cb7a1
Danial <danial.slriii@gmail.com>
2025-02-27 16:45:24 +0330

    productssamplecard before adding the img

---
+++ src/components/ProductSampleCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import BaseIcon from './icon/BaseIcon';
+const ProductSampleCard = () => {
+    return(
+        <div className='w-[288px] h-[344px] rounded-4xl flex flex-col shadow-[0px_2px_4px_0px_#1E132840]'>
+            <div className='w-[280] h-[192px]'>
+                <img src="" alt=" " />
+            </div>
+            <div className='p-4'>
+                <div className='flex flex-col w-[206px]'>
+                    <h1 className='font-semibold text-2xl leading-9 mb-2'>بوم كسب و كار</h1>
+                    <p className='font-normal text-base leading-6'>ابزاری برای تجزیه و تحلیل و بهینه سازی مدل های کسب و کار است .</p>                    
+                </div>
+                <div className="mr-auto bg-white w-12 h-12 shadow-md rounded-2xl mt-2">
+                    <div className="w-full h-full flex items-center justify-center">
+                        <BaseIcon id="Arrow" size={18}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};export default ProductSampleCard
