75876e184e864ae7bec8fc9dada88c81983b9860
Cena Babajannjhad <cenababajannjhad@gmail.com>
2025-02-27 19:39:56 +0330

    add body background color

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,11 +5,11 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <HeroSection />
       <MainPageBanner />
       <BelowMainPageBanner />
       <Footer></Footer>
-    </div>
+    </>
   );
 }
