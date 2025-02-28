b747c00e86c8611efc8290150f2ba8ad71660bfd
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-02-28 19:36:54 +0330

    Removing the Navigator and Modifying the Footer Tag on the Home Page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,6 @@ import BelowMainPageBanner from "@/components/BelowMainPageBanner";
 import HeroSection from "@/components/HeroSection";
 import MainPageBanner from "@/components/MainPageBanner";
 import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
@@ -10,7 +9,7 @@ export default function Home() {
       <HeroSection />
       <MainPageBanner />
       <BelowMainPageBanner />
-      <Footer></Footer>
+      <Footer />
     </>
   );
 }
