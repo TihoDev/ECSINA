5b506be380407dbc3a6cd3c897c76ab471204916
Cena Babajannjhad <cenababajannjhad@gmail.com>
2025-03-08 21:57:38 +0330

    change products page some logic

--- src/components/Aside/ProductAside.jsx
+++ src/components/Aside/ProductAside.jsx
@@ -20,5 +20,5 @@ const ProductAside = ({style , data}) => {
     </aside>
   )
 }
-
+ 
 export default ProductAside
--- src/components/allProductsSection/AllProductsSection.jsx
+++ src/components/allProductsSection/AllProductsSection.jsx
@@ -3,10 +3,11 @@
 import React, { useEffect, useState } from 'react'
 import ProductSampleCard from '../ProductSampleCard'
 import axios from 'axios'
-
+ 
 
 const AllProductsSection = ({style}) => {
-    const [products, setProducts]= useState()
+    const [products, setProducts] = useState([]);
+    const [isLoading , setIsLoading] = useState(true);
 
 
     const getProducts = async () => { 
@@ -15,20 +16,24 @@ const AllProductsSection = ({style}) => {
             setProducts(data.data)
         } catch (error) {
             console.log(error)
+        }finally{
+            setIsLoading(false);
         }
      }
 
     useEffect(()=>{
         getProducts()
     },[])
+
   return (
     <section className={style}>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-12 lg:gap-8 lg:grid-cols-3'>
+        <div className={`${isLoading ? "flex items-center justify-center" : "grid grid-cols-1 md:grid-cols-2 gap-12 lg:gap-8 lg:grid-cols-3"}`}>
             {
                 products?.products && products?.products?.map((product) => (
-                    <ProductSampleCard key={product.id} title={product.title} text={product.body} imageId={product.main_image}/>
+                    <ProductSampleCard key={product.id} id={product.id} title={product.title} text={product.body} imageId={product.main_image}/>
                 ))
             }
+            <div className={`${isLoading ? "block" : "hidden"} w-[40px] h-[40px] rounded-full border-4 border-black border-t-transparent animate-spin`}></div>
         </div>
     </section>
   )
