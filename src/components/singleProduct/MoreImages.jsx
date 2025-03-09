fcf3927b44a657bb5034c52abfd694cb55c0febd
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-03-09 12:14:44 +0330

    Increase the bottom spacing in the more product images section

--- src/components/singleProduct/MoreImages.jsx
+++ src/components/singleProduct/MoreImages.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MoreImages = ({ product }) => {
   return (
-    <div className="container mb-20">
+    <div className="container mb-52">
       <h2 className="text-3xl font-extrabold mb-6 text-title">تصاویر بیشتر</h2>
       <div className="grid grid-cols-2 lg:grid-cols-3 gap-10  ">
         {product?.extra_images?.slice(0, 3).map((image, index) => (
