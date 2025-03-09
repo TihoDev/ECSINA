6ec8a598475d99f30c7d64513ae8a96c66354cb2
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-03-09 12:05:59 +0330

    Improve the product page by adding product details and loading dynamic data

---
+++ src/components/singleProduct/MoreImages.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const MoreImages = ({ product }) => {
+  return (
+    <div className="container mb-20">
+      <h2 className="text-3xl font-extrabold mb-6 text-title">تصاویر بیشتر</h2>
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-10  ">
+        {product?.extra_images?.slice(0, 3).map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Image ${index + 1}`}
+            className="w-[400px] h-[300px] object-cover rounded-lg"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MoreImages;
