13956f33e34ff93cac68bf669c63f676bd688ffc
Cena Babajannjhad <cenababajannjhad@gmail.com>
2025-03-10 16:20:33 +0330

    below main page banner text changed

--- src/components/BelowMainPageBanner.jsx
+++ src/components/BelowMainPageBanner.jsx
@@ -5,8 +5,8 @@ const BelowMainPageBanner = () => {
   return (
     <section className="flex flex-col items-center gap-y-3 lg:gap-y-10 mt-[90px] lg:mt-[180px]">
       <p className=" text-title text-xs lg:text-2xl font-bold text-center leading-9">
-        اکسین به عنوان دستیاری حرفه ای همیشه همراه کسب و کار شما است .<br />{" "}
-        دغدغه ی ما هموار کردن مسیر شما است.{" "}
+      اکسین برای تأمین منابع انسانی (HR) و بخش تحقیق و توسعه (R&D)  با تیم استارپی ™️IR همکاری دارد.<br />{" "}
+        {" "}
       </p>
       <Button text={"آشنایی بیشتر"} />
     </section>
