e70627cce099ede677d6ce03254b8625e8f57cbf
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-03-10 10:31:01 +0330

    Add  Contact Us and About Us page

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="text-5xl text-red-500 flex items-center justify-center h-screen">
+      Contact
+    </div>
+  );
+};
+
+export default page;
