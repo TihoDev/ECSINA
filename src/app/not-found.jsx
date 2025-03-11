6cd7dbbfaa305d5b2c8a9e16fb8b88278c03834a
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-03-11 13:32:22 +0330

    404 page improvement: add a link back to the home page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 function notFound() {
-    return (
-        <div className="text-black text-center py-12">
-            صفحه مورد نظر پیدا نشد!
-        </div>
-    )
+  return (
+    <div className="flex flex-col justify-center items-center h-screen gap-y-5">
+      <p className="text-center text-black text-2xl">صفحه مورد نظر پیدا نشد!</p>
+      <Link href="/" className="text-center text-blue-500 text-xl">
+        بازگشت به صفحه اصلی
+      </Link>
+    </div>
+  );
 }
 
-export default notFound
+export default notFound;
