6e881f5d38e73ee20fd1c0d8b6357e5fb7a03b32
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-03-12 11:21:49 +0330

    responsive heroSection (mobile)

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -23,7 +23,7 @@ const HeroSection = () => {
               اکسین، همراهیِ نوین
             </h2>
 
-            <p className="  hidden lg:block text-xl text-description font-semibold leading-9">
+            <p className=" hidden lg:block text-xl text-description font-semibold leading-9">
               اسناد تجاری در قالب های آماده با راهنمایی کامل همه چی اینجاست تا
               کارت سریع و بی دردسر پیش بره!
             </p>
@@ -33,10 +33,13 @@ const HeroSection = () => {
               </Link>
             </div>
           </div>
-          <div className=" col-span-12 place-items-center lg:col-span-7">
-            <div className="lg:absolute  lg:left-1/8 xl:-translate-x-1/6">
+
+          <div className="relative col-span-12 flex flex-col items-center justify-center  lg:col-span-7 mt-15">
+            {/* Mobile icon */}
+            <div className="">
+              {/* lg:absolute  lg:left-1/8 xl:-translate-x-1/6 */}
               <BaseIcon
-                className="visible lg:invisible mt-15 "
+                className="visible lg:invisible  "
                 id="ExinIcon2"
                 disableGradient={true}
                 size={190}
@@ -44,6 +47,7 @@ const HeroSection = () => {
               />
             </div>
 
+            {/* Desktop */}
             <div className="invisible lg:visible">
               <div className="absolute left-11/120 -translate-x-1/24 top-[2%] ">
                 <Border size={510} />
@@ -65,6 +69,8 @@ const HeroSection = () => {
                   </div>
                 </div>
               </div>
+
+              {/* Desktop */}
               <div className="absolute left-15/120 translate-x-1/12 top-[16%] ">
                 <BaseIcon
                   className="hidden lg:block"
@@ -76,27 +82,33 @@ const HeroSection = () => {
               </div>
             </div>
 
-            <div className="lg:invisible">
-              <div className="absolute left-1/2 -translate-x-1/2 top-[15%]">
-                <Border size={300} />
-              </div>
-              <div className="absolute left-1/2 -translate-x-1/2 top-[10%]">
-                <Border size={350} />
+            {/*  mobile  Circel */}
+            <div className="lg:invisible absolute -top-11 ">
+              <div className=" flex flex-col items-center justify-center">
+                <div>
+                  <Border size={290} />
+                </div>
+                <div className="absolute">
+                  <Border size={340} />
+                </div>
               </div>
             </div>
 
-            <div className="lg:invisible text-title">
-              <div className="absolute -translate-y-8/2  -translate-x-3/2 ">
-                <Tag size={26} iconId="CrossEyesLaugh" text="بدون هزینه" />
-              </div>
-              <div className="absolute -translate-y-4/2  translate-x-7/2 ">
-                <Tag size={26} iconId="Download" text="قالب  آماده" />
-              </div>
-              <div className="absolute translate-y-2/5 translate-x-2/2   ">
-                <Tag size={26} iconId="lamp" text="راهنمایی و آموزش" />
-              </div>
-              <div className="absolute -translate-y-4/5 -translate-x-2/1  ">
-                <Tag size={26} iconId="Grow" text="قابل ویرایش" />
+            {/* Mobile  tags*/}
+            <div className="lg:invisible absolute top-0 text-title">
+              <div className="relative ">
+                <div className=" absolute -top-15 -left-30">
+                  <Tag size={26} iconId="CrossEyesLaugh" text="بدون هزینه" />
+                </div>
+                <div className="absolute top-15 left-30">
+                  <Tag size={26} iconId="Download" text="قالب  آماده" />
+                </div>
+                <div className="absolute top-50 left-5">
+                  <Tag size={26} iconId="lamp" text="راهنمایی و آموزش" />
+                </div>
+                <div className="absolute top-35 -left-40  ">
+                  <Tag size={26} iconId="Grow" text="قابل ویرایش" />
+                </div>
               </div>
             </div>
 
--- src/components/UI/Tag.jsx
+++ src/components/UI/Tag.jsx
@@ -5,7 +5,9 @@ const Tag = ({ iconId, text = "", size = 40 }) => {
   return (
     <div className="flex flex-col items-center gap-y-2 lg:gap-y-4 bg-section-background px-2 lg:px-4 py-2 lg:py-3 rounded-xl shadow-hero-section ">
       <BaseIcon id={iconId} size={size} />
-      <p className="whitespace-nowrap text-[8px] lg:text-base text-center font-bold">{text}</p>
+      <p className="whitespace-nowrap text-[10px] lg:text-base text-center font-bold">
+        {text}
+      </p>
     </div>
   );
 };
