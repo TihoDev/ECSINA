acb54a057e9948e62f2502aaef27ec53bdb74712
Cena Babajannjhad <cenababajannjhad@gmail.com>
2025-03-14 15:43:35 +0330

    I create login in wront page , fix it

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -6,14 +6,9 @@ function AdminLayout({ children }) {
   const token = true;
   if (!token) redirect("/admin/login", "replace");
 
-  return <div className="w-screen h-screen overflow-hidden bg-white">
-    <div className="pt-7 pr-7">
-      <h1 className="font-extrabold text-2xl text-black">اکسین</h1>
-    </div>
-    <div className="h-full">
-      {children}
-    </div>
-  </div>;
+  return (
+    <div>{children}</div>
+  )
 }
 
 export default AdminLayout;
--- src/app/admin/login/layout.jsx
+++ src/app/admin/login/layout.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function LoignLayout({ children }) {
-  return <div>{children}</div>;
+
+  return (<div className="w-screen h-screen overflow-hidden bg-white">
+            <div className="pt-7 pr-7">
+              <h1 className="font-extrabold text-2xl text-black">اکسین</h1>
+            </div>
+            <div className="h-full">
+              {children}
+            </div>
+        </div>
+)
 }
