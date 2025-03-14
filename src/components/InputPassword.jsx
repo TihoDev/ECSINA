68679a74c333304be17bdbb4208befb32f1099c6
Cena Babajannjhad <cenababajannjhad@gmail.com>
2025-03-14 18:53:41 +0330

    change the password eye icon

--- src/components/InputPassword.jsx
+++ src/components/InputPassword.jsx
@@ -10,7 +10,7 @@ const InputPassword = () => {
         <input type={isPassword ? "password" : "text"} className='bg-white block border-1 w-full border-[#D2D2D2] rounded-lg placeholder:text-[#8C9197] text-black py-3 px-2.5 font-normal text-sm focus:outline-0' placeholder='رمز عبور'/>
 
         <button type="button" onClick={() => setIsPassword(prev => !prev)}>
-            <BaseIcon id={isPassword ? "EyeThin" : "EyeOff"} className='absolute left-3 top-0 translate-y-2/9 cursor-pointer' disableGradient={true} fillColor='#000'/>
+            <BaseIcon id={isPassword ? "EyeOff" : "EyeThin"} className='absolute left-3 top-0 translate-y-2/9 cursor-pointer' disableGradient={true} fillColor='#000'/>
         </button>
     </div>
   )
