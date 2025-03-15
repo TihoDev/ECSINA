0c3e6c74efd32976b2e4e6c4244e871f36c4ee19
Cena Babajannjhad <cenababajannjhad@gmail.com>
2025-03-15 14:03:02 +0330

    modal created

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -3,9 +3,6 @@ import Modal from "@/components/UI/Modal"
 const page = () => {
   return (
     <div>
-      {/* <Modal modalTitle="تصاویر">
-        hell
-      </Modal> */}
       admin
     </div>
   )
