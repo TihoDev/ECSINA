59936ebdefed3af3939678712e5c5072e8c21e7a
AbolfazlAdhami <aadhami880@gmail.com>
2025-03-24 06:50:21 +0330

    feat: add adminPanel

---
+++ src/app/admin/proposal/[id]/page.jsx
@@ -0,0 +1,8 @@
+import AdminPanel from "@/components/AdminPanel/AdminPanel";
+import React from "react";
+
+function EditProposal() {
+  return <AdminPanel>page</AdminPanel>;
+}
+
+export default EditProposal;
