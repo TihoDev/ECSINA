1d8464d53b34c621c7b603733d019ecdab29bf93
AbolfazlAdhami <aadhami880@gmail.com>
2025-03-29 03:08:08 +0330

    feat: Login Page

--- src/app/admin/login/page.jsx
+++ src/app/admin/login/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import AuthForm from "@/components/authForm/AuthForm";
 import Input from "@/components/authForm/Input";
-import InputPassword from "@/components/authForm/InputPassword";
+import ErrorMessage from "@/components/authForm/ErrorMessage";
 
 export default function Login() {
   const {
@@ -18,15 +18,15 @@ export default function Login() {
 
   return (
     <AuthForm title="ورود به حساب" onSubmit={handleSubmit(onSubmit)}>
+      <Input placeholder="نام کاربری" register={register("username", { required: "نام کاربری الزامی است" })} name="username" errors={errors.username} />
+      {errors.username && <ErrorMessage message={errors?.username.message} />}
       <Input
-        placeholder="نام کاربری"
-        className="bg-white block border w-full border-[#D2D2D2] rounded-lg placeholder:text-[#8C9197] text-black py-3 px-2.5 font-normal text-sm focus:outline-0"
-        register={register}
-        name="username"
-        rules={{ required: "نام کاربری الزامی است" }}
-        errors={errors}
+        type="password"
+        register={register("password", { required: "رمز عبور الزامی است", minLength: { value: 8, message: "رمز عبور باید حداقل ۸ کاراکتر باشد" } })}
+        name={"password"}
+        errors={errors.password}
       />
-      <InputPassword register={register} errors={errors} />
+      {errors.password && <ErrorMessage message={errors?.password.message} />}
     </AuthForm>
   );
 }
