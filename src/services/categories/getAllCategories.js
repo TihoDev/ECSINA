17b0561e7ae2f9ea01a15195121a77322f884bb9
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-04-05 08:59:33 +0330

    Refactor Servisec

--- src/services/categories/getAllCategories.js
+++ src/services/categories/getAllCategories.js
@@ -1,29 +1,20 @@
 import axios from "axios";
 import { API_ENDPOINTS } from "@/constants/baseUrl";
+import { handleAxiosError } from "@/utils/axiosHelpers";
 
 export const getAllCategories = async () => {
   try {
-    const { data } = await axios.get(API_ENDPOINTS.CATEGORIES.ALL);
-    return data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      if (error.response) {
-        switch (error.response.status) {
-          case 404:
-            throw new Error("Categories not found");
-          case 401:
-            throw new Error("Unauthorized access");
-          default:
-            throw new Error(`Server error: ${error.response.status}`);
-        }
-      } else if (error.request) {
-        throw new Error("Failed to connect to server");
-      }
-    }
+    const response = await axios.get(API_ENDPOINTS.CATEGORIES.ALL, API_CONFIG);
 
-    console.error("Error fetching categories:", error);
-    throw error;
+    return {
+      data: response.data,
+      error: null,
+    };
+  } catch (error) {
+    const errorMessage = handleAxiosError(error);
+    return {
+      data: null,
+      error: errorMessage,
+    };
   }
 };
-
-export default getAllCategories;
