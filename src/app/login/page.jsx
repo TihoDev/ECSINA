fa8a1802d1128584fd027f346421c6fff54059b9
Danial <danial.slriii@gmail.com>
2025-04-07 13:47:08 +0330

    added react hook form and KIS DRY

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,14 +1,28 @@
-import React from "react";
-import LoginCard from "@/components/LoginCard";
-import LoginCard2 from "@/components/LoginCard2";
-const page = () => {
+"use client"
+import React, {useState} from "react";
+import PhoneLoginForm from "@/components/Login/PhoneLoginForm";
+import CodeVerificationForm from "@/components/Login/CodeVerificationForm";
+const loginPage = () => {
+  const [step, setStep] = useState("phone");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handlePhoneSubmitSuccess = (enteredPhone) => {
+    setPhoneNumber(enteredPhone);
+    setStep("code");
+  };
+  const handleBackToPhoneLogin = () => {
+    setStep("phone"); // Go back to the phone login form
+  };
+
   return (
-    <div className="flex justify-center items-center h-[2000px] flex-col gap-10 ">
-        <LoginCard btntext={'ارسال کد'}></LoginCard>
-        <LoginCard2></LoginCard2>
+    <div className="flex justify-center items-center h-full flex-col gap-10 ">
+        {step === "phone" && (
+        <PhoneLoginForm buttonText="ارسال کد" onSuccess={handlePhoneSubmitSuccess} />
+      )}
+      {step === "code" && <CodeVerificationForm phoneNumber={phoneNumber} onBack={handleBackToPhoneLogin}/>}
     </div>
     
   );
 };
 
-export default page;
+export default loginPage;
