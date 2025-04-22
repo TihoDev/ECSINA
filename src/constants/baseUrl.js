188b2192c4235961ce0f49826b5399c6d076adc8
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-04-22 14:37:26 +0330

    import  api from env

--- src/constants/baseUrl.js
+++ src/constants/baseUrl.js
@@ -1,5 +1,6 @@
-export const BASE_URL = "https://api.exin.exiness.com";
-export const API_VERSION = "api";
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_VERSION = process.env.NEXT_PUBLIC_API_VERSION;
+
 export const API_CONFIG = {
   headers: {
     "Content-Type": "application/json",
