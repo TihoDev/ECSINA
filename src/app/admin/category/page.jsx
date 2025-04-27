fa31607e8ed8b1c3cca9e561197faad6f01ae14e
AbolfazlAdhami <aadhami880@gmail.com>
2025-04-28 01:01:58 +0330

    feat: aside admin done

--- src/app/admin/category/page.jsx
+++ src/app/admin/category/page.jsx
@@ -1,8 +1,14 @@
 import AdminPanel from "@/components/AdminPanel/AdminPanel";
-import React from "react";
+import Title from "@/components/UI/Title";
+import React, { useState } from "react";
 
 function Category() {
-  return <AdminPanel></AdminPanel>;
+  const [isUpdate, setIsUpdate] = useState(false);
+  return (
+    <AdminPanel>
+      <Title text={"مدیریت دسته بندی ها"} className="font-bold text-2xl" />
+    </AdminPanel>
+  );
 }
 
 export default Category;
