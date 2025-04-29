a605e30aac1b175c136b1ac2147f2ac2429bb54a
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-04-29 10:14:55 +0330

    refactor: enhance useCategories hook with loading state and error handling

--- src/hooks/useCategories.js
+++ src/hooks/useCategories.js
@@ -1,24 +1,29 @@
-import { useState, useEffect } from "react";
 import { getAllCategories } from "@/services/categories/getAllCategories";
+import { useState, useEffect, useCallback } from "react";
 
 const useCategories = () => {
   const [categories, setCategories] = useState([]);
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const { data } = await getAllCategories();
-        setCategories(data);
-      } catch (error) {
-        setError(error);
-      }
-    };
-
-    fetchCategories();
+  const fetchCategories = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const { data } = await getAllCategories();
+      setCategories(data);
+      setError(null);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
   }, []);
 
-  return { categories, error };
+  useEffect(() => {
+    fetchCategories();
+  }, [fetchCategories]);
+
+  return { categories, error, isLoading };
 };
 
 export default useCategories;
