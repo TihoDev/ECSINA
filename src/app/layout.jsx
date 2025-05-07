b9cb89f879f0487e359afe7d39330d4f061809d4
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-05-07 17:55:13 +0330

    Update: Removed daisyui plugin from globals.css, added container class to body in layout.jsx, and created HeroBanner component with new content in page.jsx and HeroBanner.jsx.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,7 +13,7 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body>{children}</body>
+      <body className="container">{children}</body>
     </html>
   );
 }
--- src/components/home/HeroBanner.jsx
+++ src/components/home/HeroBanner.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import Button from "../UI/Button";
+
+import Arrow from "../../../public/assets/icons/Arrow.svg";
 
 const HeroBanner = () => {
-  return <section></section>;
+  return (
+    <section>
+      <div className="flex flex-col items-center gap-6">
+        <h2 className="text-5xl text-black font-semibold">
+          با اکسینا، اسناد تجاری را مثل حرفه‌ای‌ها بساز!
+        </h2>
+        <h6 className="text-3xl  font-normal text-black max-w-[60%] text-center">
+          قالب‌های آماده، راهنمای کامل، همه‌چی اینجاست تا کارت سریع و بی‌دردسر
+          پیش بره!
+        </h6>
+
+        <Button icon={Arrow}>رایگان دانلود کن</Button>
+      </div>
+    </section>
+  );
 };
 
 export default HeroBanner;
-
