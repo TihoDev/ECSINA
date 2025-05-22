7bfb16f1b0c5a576a863d0f6b492349ad7c88d16
AbolfazlAdhami <aadhami880@gmail.com>
2025-05-22 19:37:29 +0330

    new: auth page

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,8 +1,15 @@
-
+import Image from "next/image";
 import React from "react";
 
 const SignupPage = () => {
-  return <div className="flex justify-center items-center min-w-screen min-h-screen "></div>;
+  return (
+    <div className="flex justify-center items-center min-w-screen min-h-screen relative">
+      <Image alt="" src={"/assets/images/bg-vector.svg"} fill className="" />
+      <section className="w-2/3 h-4/5 blue_shadow p-0.5">
+        <div className="w-fit h-fit  bg-white">dawdwdwa</div>
+      </section>
+    </div>
+  );
 };
 
 export default SignupPage;
