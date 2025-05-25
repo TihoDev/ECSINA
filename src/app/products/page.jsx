be9e211dc1c5163d7f7d27ee672a6960f4ad3b5e
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-05-25 13:44:34 +0330

    Updated the products page by adding search, button, and category components. Improved code structure and added new sections to display categories and products.

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,9 +1,37 @@
 import MainLayout from "@/components/layout/MainLayout";
+import Button from "@/components/UI/Button";
+import AllCategories from "@/components/products/AllCategories";
+import SearchInput from "@/components/UI/SearchInput";
+import AllProducts from "@/components/products/AllProducts";
 
 export default function Page() {
   return (
     <MainLayout>
-      <div className="container">Products</div>
+      <section className="gradient-main-background mt-4 md:mt-16 ">
+        <div className="container py-16 md:py-[110px]">
+          {/* Header */}
+          <div className="flex items-center justify-between">
+            <SearchInput placeholder={"جست و جو ..."} />
+            <Button>راهنمای دانلود قالب</Button>
+          </div>
+          {/* Categories */}
+          <div className=" mt-16 md:mt-30">
+            <h3 className="text-black text-base md:text-4xl font-bold flex items-center justify-center">
+              دسته‌بندی محصولات
+            </h3>
+            <AllCategories />
+          </div>
+          {/* Products */}
+
+          <div className=" mt-16 md:mt-30">
+            <h3 className="text-black text-base md:text-4xl font-bold flex items-center justify-center">
+              جدیدترین قالب‌ها
+            </h3>
+
+            <AllProducts />
+          </div>
+        </div>
+      </section>
     </MainLayout>
   );
 }
