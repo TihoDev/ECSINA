03ed4c2148a73ea6c5c45547004116852465b4c4
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-05-25 21:25:35 +0330

    Updated the products page by removing the button component and replacing it with a custom button. Also, the spacing between elements in the header has been improved.

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,5 +1,4 @@
 import MainLayout from "@/components/layout/MainLayout";
-import Button from "@/components/UI/Button";
 import AllCategories from "@/components/products/AllCategories";
 import SearchInput from "@/components/UI/SearchInput";
 import AllProducts from "@/components/products/AllProducts";
@@ -10,9 +9,11 @@ export default function Page() {
       <section className="gradient-main-background mt-4 md:mt-16 ">
         <div className="container py-16 md:py-[110px]">
           {/* Header */}
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between gap-6">
             <SearchInput placeholder={"جست و جو ..."} />
-            <Button>راهنمای دانلود قالب</Button>
+            <button className="bg-primary-7 rounded-xl md:rounded-2xl text-white text-nowrap py-3 px-5 md:py-5  lg:px-11 text-xs lg:text-2xl font-semibold cursor-pointer hover:bg-primary-8 transition-all duration-200 ">
+              راهنمای دانلود قالب
+            </button>
           </div>
           {/* Categories */}
           <div className=" mt-16 md:mt-30">
