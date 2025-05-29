8f45a839ac353f288470f7cb13f9f08ef31618d0
AbolfazlAdhami <aadhami880@gmail.com>
2025-05-29 16:41:24 +0330

    new: 404 page

--- src/app/[...not-found]/page.jsx
+++ src/app/[...not-found]/page.jsx
@@ -1,14 +1,26 @@
+import Button from "@/components/UI/Button";
+import Image from "next/image";
 import Link from "next/link";
 
-function notFound() {
+function NotFound() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen gap-y-5">
-      <p className="text-center text-black text-2xl">صفحه مورد نظر پیدا نشد!</p>
-      <Link href="/" className="text-center text-blue-500 text-xl">
-        بازگشت به صفحه اصلی
-      </Link>
-    </div>
+    <body>
+      <main className="flex flex-col md:flex-row  h-screen w-screen items-center justify-center gap-5">
+        <div className="w-[300px] md:w-[590px] h-[226px] md:h-[440px] object-contain relative flex items-center justify-center">
+          <Image src={"/assets/icons/MainLogo.svg"} alt="Main Logo" fill className="w-full h-full object-contain" />
+        </div>
+        <div className="flex flex-col gap-0 h-fit md:border-r-4 pr-3 border-r-black justify-start items-center md:items-start p-1  ">
+          <h1 className="md:text-9xl text-5xl">404</h1>
+          <p className="text-center md:-mt-6 text-2xl font-semibold">متاسفانه صفحه مورد نظر شما پیدا نشد.</p>
+          <Button className="mt-6">
+            <Link href="/" className="">
+              بازگشت به صفحه اصلی
+            </Link>
+          </Button>
+        </div>
+      </main>
+    </body>
   );
 }
 
-export default notFound;
+export default NotFound;
