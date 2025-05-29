4b65fc58abb88f3c1c34eda6b868c675d85843fe
AbolfazlAdhami <aadhami880@gmail.com>
2025-05-29 23:47:06 +0330

    new: 403 page

--- src/app/403/page.jsx
+++ src/app/403/page.jsx
@@ -2,7 +2,7 @@ import Button from "@/components/UI/Button";
 import Image from "next/image";
 import Link from "next/link";
 
-function NotFoundPage() {
+function ForbiddenPage() {
   return (
     <body>
       <main className="flex flex-col md:flex-row  h-screen w-screen items-center justify-center gap-5">
@@ -10,8 +10,8 @@ function NotFoundPage() {
           <Image src={"/assets/icons/MainLogo.svg"} alt="Main Logo" fill className="w-full h-full object-contain" />
         </div>
         <div className="flex flex-col gap-0 h-fit md:border-r-4 pr-3 border-r-black justify-start items-center md:items-start p-1  ">
-          <h1 className="md:text-9xl text-5xl">404</h1>
-          <p className="text-center md:-mt-6 text-2xl font-semibold">متاسفانه صفحه مورد نظر شما پیدا نشد.</p>
+          <h1 className="md:text-9xl text-5xl">403</h1>
+          <p className="text-center md:-mt-6 text-2xl font-semibold">شما اجازه دسترسی به این بخش را ندارید.</p>
           <Button className="mt-6">
             <Link href="/" className="">
               بازگشت به صفحه اصلی
@@ -23,4 +23,4 @@ function NotFoundPage() {
   );
 }
 
-export default NotFoundPage;
+export default ForbiddenPage;
