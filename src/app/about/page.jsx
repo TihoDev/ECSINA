31f79c154e5456da2e22a49b8404cff9edd624cf
AbolfazlAdhami <aadhami880@gmail.com>
2025-06-06 02:51:53 +0330

    new: featur list about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -15,52 +15,47 @@ export const metadata = {
   },
 };
 
+const featurList = [
+  { title: "آموزش‌ همراه قالب‌ها", desc: ".همه قالب‌ها همراه با آموزش‌های متنی و ویدئویی ارائه می‌شوند تا کاربر با نحوه استفاده صحیح و اصولی از آن‌ها آشنا شود" },
+  { title: "نمونه‌های تکمیل‌شده", desc: "برای درک بهتر ساختار و نحوه تکمیل اسناد، نمونه‌هایی آماده در دسترس کاربران قرار دارد." },
+  { title: "قالب‌های آماده", desc: "مجموعه‌ای متنوع از قالب‌های طراحی‌شده " },
+  { title: "پیش‌نمایش و انتخاب هوشمند", desc: "برای درک بهتر ساختار و نحوه تکمیل اسناد، نمونه‌هایی آماده در دسترس کاربران قرار دارد." },
+];
+
 const AboutPage = () => {
   return (
     <MainLayout>
-      <section className="flex flex-col  items-center my-5   min-w-screen">
+      <section className="my-5  gradient_about   min-w-screen">
         <div className="container my-8 p-5 py-12 relative shadow-2xl  md:shadow-none  rounded-lg">
-          <div className="z-20 top-20 left-10 w-40 absolute">
-            <Image src={"/assets/icons/Star.svg"} alt="Star" width={20} height={20} />
+          <div className="z-20  w-[10px] md:w-[35px] md:h-[35px] h-[10px] left-36   absolute">
+            <Image src={"/assets/icons/Star.svg"} alt="Star" fill />
           </div>
-          <div className="z-20 absolute">
-            <Image src={"/assets/icons/Star.svg"} alt="Star" width={20} height={20} />
+          <div className="z-20 absolute w-[10px] md:w-[35px] md:h-[35px] h-[10px] right-[130px] top-[130px]">
+            <Image src={"/assets/icons/Star.svg"} alt="Star" fill />
           </div>
           {/* <Image src={"/assets/icons/MainLogo.svg"} alt="Main Logo" width={600} height={550} className="z-20" /> */}
-          <h1 className="text-black  text-center text-4xl mb-12 font-bold pt-8">درباره اکسینا</h1>
-          <div>
-            <h2 className="font-extralight text-xl text-center">اکسین، سکویی برای هوشمندسازی تولید اسناد کسب‌وکار</h2>
-            <Typography className={"font-extralight text-xl   text-wrap text-center "}>
+          <h1 className="text-black  text-center text-2xl md:text-4xl mb-12 font-bold pt-8">درباره اکسینا</h1>
+          <div className="text-balance px-8 md:px-2">
+            <h2 className="font-extralight text-base md:text-xl text-center">اکسین، سکویی برای هوشمندسازی تولید اسناد کسب‌وکار</h2>
+            <Typography className={"font-extralight text-base md:text-xl    text-wrap text-center "}>
               اکسین یک پلتفرم نوآورانه است که با هدف ساده‌سازی فرآیند تولید اسناد کسب‌وکار طراحی شده است. ما در اکسین تلاش کرده‌ایم بستری فراهم کنیم که کاربران حتی با کمترین  دانش تخصصی در این زمینه
               بتوانند اسناد مهمی مانند بیزینس پلن، بوم مدل کسب‌وکار، پروپوزال، بیزینس مدل، بوم ناب و... را به شکلی کاملاً حرفه‌ای تهیه و ارائه کنند.
             </Typography>
           </div>
-          {/* <div className="mt-8">
-            <h1 className="text-black text-3xl font-bold text-center pt-8">اهداف اکسینا</h1>
-            <div className="grid gap-5 md:gap-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  my-6 px-4 sm:px-8">
-              <div className=" border-1 border-primary-8 rounded-4xl p-4 box-gradient hover:shadow-lg transition-all ease-linear duration-500 py-8">
-                <h1 className="text-black text-2xl font-bold text-center my-3">اهداف اکسینا</h1>
-                <Typography className={"text-base text-right"}>
-                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
-                  تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.
-                </Typography>
+        </div>
+        <div className="mt-24 md:mt-48 pb-36 container">
+          <div></div>
+          <div className="grid gap-5 md:gap-10 grid-cols-1 lg:grid-cols-2  my-6 px-4 sm:px-8">
+            {featurList.map((item) => (
+              <div
+                key={item.title}
+                className="h-80 flex items-center justify-evenly  flex-col bg-secondary-2 border border-primary-7 rounded-4xl p-4  hover:shadow-lg transition-all ease-linear duration-500 py-8"
+              >
+                <h1 className="text-black text-2xl font-bold text-center my-3">{item.title}</h1>
+                <Typography className={"text-lg text-center leading-10 px-10"}>{item.desc}</Typography>
               </div>
-              <div className=" border-1 border-primary-8 rounded-4xl p-4 box-gradient hover:shadow-lg transition-all ease-linear duration-500 py-8">
-                <h1 className="text-black text-2xl font-bold text-center my-3">اهداف اکسینا</h1>
-                <Typography className={"text-base text-right"}>
-                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
-                  تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.
-                </Typography>
-              </div>
-              <div className=" border-1 border-primary-8 rounded-4xl p-4 box-gradient hover:shadow-lg transition-all ease-linear duration-500 py-8">
-                <h1 className="text-black text-2xl font-bold text-center my-3">اهداف اکسینا</h1>
-                <Typography className={"text-base text-right"}>
-                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
-                  تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.
-                </Typography>
-              </div>
-            </div>
-          </div> */}
+            ))}
+          </div>
         </div>
       </section>
     </MainLayout>
