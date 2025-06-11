eb27bb8631206768efbb97d2c3deb07beb5b9ede
AbolfazlAdhami <aadhami880@gmail.com>
2025-06-11 23:47:20 +0330

    update: delete txt file

--- src/app/products/GuideButton.jsx
+++ src/app/products/GuideButton.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { Button, Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
 import { IoCloseSharp } from "react-icons/io5";
 
 function GuideButton() {
