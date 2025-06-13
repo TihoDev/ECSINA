833402ff643e156a8f43b23665a867570a90bf57
AbolfazlAdhami <aadhami880@gmail.com>
2025-06-13 19:51:33 +0330

    update: singup layout page

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,3 +1,4 @@
+import SignupForm from "@/components/auth/SignupForm";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -5,29 +6,42 @@ import { IoClose } from "react-icons/io5";
 
 const SignupPage = () => {
   return (
-    <div className="flex justify-center items-center  w-screen  h-screen relative">
-      {/* <Image alt="" src={"/assets/images/bg-vector.svg"} fill className="" /> */}
-      <section className="w-2/3 h-4/5 blue_shadow rounded-xl p-4 flex flex-col items-center justify-start ">
-        <div className="w-full flex justify-between items-center ">
-          <div className="relative size-7 md:size-21">
+    <main className="flex flex-col justify-evenly sm:justify-center items-center z-0 w-screen  h-screen relative">
+      <div className="flex-col md:hidden text-white items-center z-10 ">
+        <h1 className="text-xl text-center">سلام!</h1>
+        <p className="mt-3  text-xl">
+          به&nbsp;
+          <Link className="p-0.5" href={"/"}>
+            اکسینا
+          </Link>
+          خوش آمدید.
+        </p>
+      </div>
+      <div className="w-full sm:hidden h-[250px] absolute z-0 top-0">
+        <Image alt="" src={"/assets/images/sign-up.png"} fill />
+      </div>
+      <section className="w-2/3 h-4/5 relative bg-white z-10  blue_shadow  rounded-xl p-4 flex flex-col items-center justify-start ">
+        <div className="w-full flex justify-end sm:justify-between  items-center  z-40">
+          <div className="relative  size-12 md:size-21">
             <Image src={"/assets/icons/Logo.svg"} alt="Logo" fill />
           </div>
-          <div className="hidden md:flex md:flex-col">
+          <div className="hidden md:flex items-center justify-start md:flex-col">
             <h1 className="text-5xl">سلام!</h1>
-            <p>
-              به
-              <Link className="p-0.5" href={"/"}>
+            <p className="mt-3 font-semibold text-2xl">
+              به&nbsp;
+              <Link className="p-1 text-primary-7" href={"/"}>
                 اکسینا
               </Link>
               خوش آمدید.
             </p>
           </div>
-          <Link href={"/"}>
-            <IoClose size={20} />
+          <Link href={"/"} className="p-6 absolute -top-32 left-0  sm:block">
+            <IoClose className="sm:text-black  text-white hover:opacity-60" size={25} />
           </Link>
         </div>
+        <SignupForm />
       </section>
-    </div>
+    </main>
   );
 };
 
---
+++ src/components/auth/SignupForm.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function SignupForm() {
+  return <form>
+    
+  </form>;
+}
+
+const CustomInput = ({ name, id }) => {
+  return (
+    <div>
+      <label>{name}</label>
+      <input type="type" />
+    </div>
+  );
+};
+
+export default SignupForm;
