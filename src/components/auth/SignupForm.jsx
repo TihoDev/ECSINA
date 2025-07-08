e4821ba2244fc39f44c0cd57d8db725c614d50cf
AbolfazlAdhami <aadhami880@gmail.com>
2025-07-08 12:44:52 +0330

    feat(auth): setup from and inputes

--- src/components/auth/SignupForm.jsx
+++ src/components/auth/SignupForm.jsx
@@ -1,6 +1,8 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import Button from "../UI/Button";
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from "@headlessui/react";
 
 function SignupForm() {
   const {
@@ -10,21 +12,52 @@ function SignupForm() {
     formState: { errors },
   } = useForm();
 
-  const inputesFueild = [{ name: "", type: "" }];
+  const inputesField = [
+    { name: "نام و نام خانوادگی", type: "text", title: "", required: false },
+    { name: "شماره تماس", type: "text", title: "", required: false },
+    { name: "ایمیل", type: "text", title: "", required: false },
+    { name: "رمز عبور", type: "text", title: "", required: false },
+  ];
+  const selectBoxFields = [
+    { name: "نوع کسب کار", type: "text", title: "", options: [] },
+    { name: "سمت شغلی", type: "text", title: "", options: [] },
+    { name: "حوزه فعالیت :", type: "text", title: "", options: [] },
+    { name: "چطور با ما آشنا شدید؟", type: "text", title: "", options: [] },
+  ];
+
   const onSubmit = (data) => console.log(data);
 
-  return <form onSubmit={handleSubmit(onSubmit)}>
-
-    
-  </form>;
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className=" w-full flex flex-col justify-center items-center w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 place-content-center place-items-center"></div>
+      <Button className="p-4  col-span-1 text-2xl  self-center">ثبت نام</Button>
+    </form>
+  );
 }
 
-const CustomInput = ({ name, id }) => {
+const CustomInput = ({ name, id, type, title, ...props }) => {
   return (
-    <div>
+    <div className="fexl flex-col items-start gap-1">
       <label>{name}</label>
-      <input type="type" />
+      <input type={type} required />
     </div>
+  );
+};
+
+const CustomSelextBox = ({}) => {
+  const [selectedPerson, setSelectedPerson] = useState(people[0]);
+
+  return (
+    <Listbox value={selectedPerson} onChange={setSelectedPerson}>
+      <ListboxButton>{selectedPerson.name}</ListboxButton>
+      <ListboxOptions anchor="bottom">
+        {options.map((option) => (
+          <ListboxOption key={option.id} value={option} className="data-focus:bg-blue-100">
+            {option.name}
+          </ListboxOption>
+        ))}
+      </ListboxOptions>
+    </Listbox>
   );
 };
 
